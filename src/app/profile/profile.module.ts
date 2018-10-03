3e19b0dd4944aaf73bf188c1e77d31a1686faef7
Vlasenko Elena <vlas.lvv@gmail.com>
2018-10-03 16:39:15 +0300

    edit style in cimponents

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile/profile.component';
 import { ProfileRoutingModule } from './profile.routing.module';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    ProfileRoutingModule
+    ProfileRoutingModule,
+    MaterialModule
   ],
   declarations: [ProfileComponent]
 })
