19961097f0708f7927200aacf536cfef7cc2021c
Vlasenko Elena <vlas.lvv@gmail.com>
2018-10-08 11:01:56 +0300

    add formBuilder in logincomponent, registrationcomp

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -1,51 +1,66 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
-import { AuthService } from '../../shared/services/auth.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormControl,
+  Validators,
+  FormGroup,
+  FormBuilder
+} from "@angular/forms";
+import { AuthService } from "../../shared/services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-registration',
-  templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  selector: "app-registration",
+  templateUrl: "./registration.component.html",
+  styleUrls: ["./registration.component.css"]
 })
 export class RegistrationComponent implements OnInit {
-
-  constructor(private authService: AuthService, private router: Router) { }
-
   registrationForm: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit() {
     this.signIn();
   }
 
   signIn() {
-    this.registrationForm = new FormGroup ({
-      'email':  new FormControl('', [Validators.required, Validators.email]),
-      'password': new FormControl('', [Validators.required, Validators.minLength(8)]),
-      'name': new FormControl('', [Validators.required])
-    })
+    this.registrationForm = this.fb.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(9)]],
+      name: ["", [Validators.required]]
+    });
+    console.log(this.registrationForm);
   }
-  
-   registrationFormInfo() {
-     let registrationInfo = this.registrationForm.value;
-     console.log(registrationInfo);
-     
-     if (registrationInfo) {
-       this.authService.createNewUser(registrationInfo)
-       .subscribe(
-        (user) => {
-          this.router.navigate(['posts/my-post'])
+
+  registrationFormInfo() {
+    let registrationInfo = this.registrationForm.value;
+    console.log(registrationInfo);
+
+    if (registrationInfo) {
+      this.authService.createNewUser(registrationInfo).subscribe(
+        user => {
+          this.router.navigate(["posts/my-post"]);
           console.log(user);
         },
-        (err) => console.log(err)); 
-     }
-    
+        err => console.log(err)
+      );
+    }
   }
 
-  /*getErrorMessage() {
-    return this.email.hasError('required') ? 'You must enter a value' :
-        this.email.hasError('email') ? 'Not a valid email' :
-            '';
-  }*/
+  getErrorMessage() {
+    return this.registrationForm.get("email").hasError("required")
+      ? "You must enter a value"
+      : this.registrationForm.get("email").hasError("email")
+        ? "Not a valid email"
+        : "";
+  }
 
+  getErrorMessagePas() {
+    return this.registrationForm.get("password").hasError("minlength")
+      ? "Password must be at least 9 characters long."
+      : "";
+  }
 }
