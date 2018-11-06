a2fb78d03cc424972f51c977a30aa5a5e89db2e1
Vlasenko Elena <vlas.lvv@gmail.com>
2018-11-06 11:35:50 +0200

    new karma

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -26,13 +26,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     customLaunchers: {
       ChromeHeadless: {
         base: 'Chrome',
         flags: ['--headless', '--disable-gpu', '--no-sandbox', '--disable-extensions', '--remote-debugging-port=9222']
       }
     },
-    singleRun: false
+    singleRun: true
   });
 };
